58d22e91413cbdf6ad224c8426a50946928e124d
RayleighZ <3226139433@qq.com>
2021-04-07 01:54:41 +0800

    [release]迪杰斯特拉算法

--- main.c
+++ main.c
@@ -33,12 +33,41 @@ struct PathLink {
     pathLink *next;
 };
 
+void showPath(pathLink *path) {
+    pathLink *cursor = path;
+    while (cursor != NULL) {
+        unsigned int length = strlen(cursor->path);
+        for (int i = 0; i < length - 1; ++i) {
+            printf("%c --> ", cursor->path[i] + 1);
+        }
+        printf("%c\n", cursor->path[length - 1] + 1);
+        cursor = cursor->next;
+    }
+}
+
 edge *edgeBuilder(node *lightNode, node *darkNode, int dis) {
     edge *_edge = (edge *) malloc(sizeof(edge));
     _edge->length = dis;
     _edge->darkSide = darkNode;
     _edge->lightSide = lightNode;
     return _edge;
+}
+
+//整数转型为字符串
+char *int2String(int mark) {
+    int length = 0;
+    int cMark = mark;
+    do {
+        cMark /= 10;
+        length++;
+    } while (cMark != 0);
+    char *string = (char *) malloc(sizeof(char) * length);
+    string[length] = 0;
+    for (int i = length - 1; i >= 0; --i) {
+        string[i] = mark % 10 + '0';
+        mark /= 10;
+    }
+    return string;
 }
 
 nodeArrayWrapper *initNodeArray() {
@@ -68,75 +97,103 @@ nodeArrayWrapper *initNodeArray() {
         int node1Id = atoi(node1Id_C);
         int node2Id = atoi(node2Id_C);
         int distance = atoi(distance_C);
-        node node1 = naw->nodeArray[node1Id];
-        node node2 = naw->nodeArray[node2Id];
-        node1.id = node1Id;
-        node2.id = node2Id;
-        node1.edgeArray[node1.edgeCount] = *edgeBuilder(&node1, &node2, distance);
-        node2.edgeArray[node2.edgeCount] = *edgeBuilder(&node2, &node1, distance);
+        naw->nodeArray[node1Id - 1].id = node1Id - 1;
+        naw->nodeArray[node2Id - 1].id = node2Id - 1;
+        naw->nodeArray[node1Id - 1].flag = 0;
+        naw->nodeArray[node2Id - 1].flag = 0;
+        naw->nodeArray[node1Id - 1].edgeArray[naw->nodeArray[node1Id - 1].edgeCount] = *edgeBuilder(
+                &naw->nodeArray[node1Id - 1], &naw->nodeArray[node2Id - 1], distance);
+        naw->nodeArray[node2Id - 1].edgeArray[naw->nodeArray[node2Id - 1].edgeCount] = *edgeBuilder(
+                &naw->nodeArray[node2Id - 1], &naw->nodeArray[node1Id - 1], distance);
 
-        node1.edgeCount++;
-        node2.edgeCount++;
+        naw->nodeArray[node1Id - 1].edgeCount++;
+        naw->nodeArray[node2Id - 1].edgeCount++;
     }
     return naw;
 }
 
-int isAllOne(const int *array, int size) {
-    for (int i = 0; i < size; ++i) {
-        if (!array[i]) {
-            return 0;
-        }
-    }
-    return 1;
-}
 
-void dijkstra(nodeArrayWrapper *naw, int startNodeIndex) {
+pathLink *dijkstra(nodeArrayWrapper *naw, int startNodeIndex) {
 
     //初始化最短路数组
-    pathLink *minPaths = (pathLink *)malloc(sizeof(pathLink) * naw->size);
+    pathLink *minPaths = (pathLink *) malloc(sizeof(pathLink) * naw->size);
     for (int i = 0; i < naw->size; ++i) {
-        minPaths->path = (char *)malloc(sizeof(char)*naw->size);
+        minPaths[i].path = (char *) malloc(sizeof(char) * naw->size);
+        minPaths[i].next = NULL;
     }
     minPaths[startNodeIndex].path[0] = startNodeIndex + '0';
     minPaths[startNodeIndex].path[1] = 0;
 
     //声明计算用数组
     int length[naw->size];//缓存最大长度的数组
-    length[0] = 0;//到自身的距离为0
-    int blueJudgeArray[naw->size];//缓存是否加入蓝色节点队列的数组
-    for (int i = 0; i < naw->size; ++i) {
-        blueJudgeArray[i] = 0;
-    }
-    blueJudgeArray[startNodeIndex] = 1;
-
-    node *nodeArray = naw->nodeArray;
     for (int i = 1; i < naw->size; ++i) {
         length[i] = INT_MAX;
     }
+    length[startNodeIndex] = 0;//到自身的距离为0
 
+    node *nodeArray = naw->nodeArray;
+    nodeArray[startNodeIndex].flag = 1;
     node currentBlueNode = nodeArray[startNodeIndex];
-
-    while (!isAllOne(blueJudgeArray, naw->size)) {
+    int blueNodeCount = 0;
+    while (blueNodeCount < naw->size) {
+        //首先根据cbn刷新到达起始点的最短路径
         for (int i = 0; i < currentBlueNode.edgeCount; ++i) {
-            edge curEdge = currentBlueNode.edgeArray[i];
             node *darkSide = currentBlueNode.edgeArray[i].darkSide;
-            int refreshLength = curEdge.length + length[currentBlueNode.id];
-            if (refreshLength < length[darkSide->id]) {
-                //认定为可刷新最短路径
-                length[darkSide->id] = refreshLength;
-                //拼接最短路
-                char darkSideMinPath[naw->size];
-                strcpy(darkSideMinPath, minPaths[currentBlueNode.id].path);//先把blueNode上的最短路径复制过来
-                char curIndex[2];
-                curIndex[1] = 0;
-                curIndex[0] = darkSide->id + '0';
-                strcat(darkSideMinPath, curIndex);
+            if (darkSide->flag){//如果已经确定了到达对应节点的最短路径，就直接跳过
+                continue;
+            }
+            //判断新节点是否可以更新最短路径
+            int curLength = currentBlueNode.edgeArray[i].length + length[currentBlueNode.id];
+            if (length[darkSide->id] >= curLength) {
+
+                //形成新的最短路径
+                char *path = (char *)malloc(sizeof(char) * naw->size);
+                strcpy(path, minPaths[currentBlueNode.id].path);
+                strcat(path, int2String(darkSide->id));//将新的最后一位（darkSide添加进去）
+                if (length[darkSide->id] == curLength) {
+                    //连接minPaths darkSide位置的链表
+                    pathLink * pathLink_newMin = (pathLink *)malloc(sizeof(pathLink));
+                    pathLink_newMin->next = NULL;
+                    pathLink_newMin->path = path;
+                    minPaths[darkSide->id].next = pathLink_newMin;
+                } else {//不需要拼接，直接强行刷新
+                    //刷新最短路径长度
+                    length[darkSide->id] = curLength;
+                    minPaths[darkSide->id].path = path;
+                    //释放链表后续指针所在地址
+                    pathLink * cursor = minPaths[darkSide->id].next;
+                    pathLink * freePoint;
+                    while (cursor != NULL){
+                        freePoint = cursor;
+                        cursor = cursor->next;
+                        free(freePoint);
+                    }
+                    minPaths[darkSide->id].next = NULL;//保证之前的后续路径被丢弃掉
+                }
             }
         }
+        int minToStartLength = INT_MAX;
+        int minNodeId;
+        for (int i = 0; i < naw->size; ++i) {
+            if (length[i] < minToStartLength && !nodeArray[i].flag){
+                minNodeId = i;
+                minToStartLength = length[i];
+            }
+        }
+        //再根据当前最短节点刷新cbn
+        currentBlueNode = nodeArray[minNodeId];
+        //判定为flag为1
+        nodeArray[minNodeId].flag = 1;
+        blueNodeCount++;
     }
+    return minPaths;
 }
 
 int main() {
-    nodeArrayWrapper naw = *initNodeArray();
+    nodeArrayWrapper *naw = initNodeArray();
+    pathLink *minPaths = dijkstra(naw, 3);
+    for (int i = 0; i < naw->size; ++i) {
+        showPath(&minPaths[i]);
+    }
     return 0;
 }
